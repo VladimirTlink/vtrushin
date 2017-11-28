91f8467fd2088adfdb09b4c6e46b958d7eb20a34
VladimirTlink <1link130984@rambler.ru>
2017-11-28 23:09:39 +0300

    1.3. JavaDoc.Code conversation[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,10 @@
 package ru.job4j;
 
+/**
+ *Class Calculate решение задачи части 001 урок 1.
+ *@autor vtrushin
+ *@since 28.11.2017
+*/ 
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
